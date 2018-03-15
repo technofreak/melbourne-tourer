68117b85c1a233ab44188b356938425cba24ebb5
Parthan S Ramanujam <parthan@technofreak.in>
2018-03-15 16:51:56 +1100

    removing :: added as marker

--- app/assets/javascripts/components/place.jsx
+++ app/assets/javascripts/components/place.jsx
@@ -6,7 +6,7 @@ class Place extends React.Component {
     render() {
         return (
             <div className='card col-xs-4'>
-                <h4 className='card-header'>:: {this.props.place.name}</h4>
+                <h4 className='card-header'>{this.props.place.name}</h4>
                 <p className='card-address'>{this.props.place.address}</p>
                 <p className='card-phone_number'>{this.props.place.phone_number}</p>
                 <p className='card-website'>{this.props.place.website}</p>
